e776ff082a4f8123f20b0336e0c10ab4cc628888
Cicada <hubbleadd@bk.ru>
2023-04-01 21:47:22 +0700

    Add main for checking

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,5 +1,6 @@
 #include "../cache/cache.hpp"
 
+#include <cassert>
 #include <iostream>
 
 int ret(int key)
@@ -9,4 +10,20 @@ int ret(int key)
 
 int main()
 {
+    unsigned size, elCnt;
+
+    std::cin >> size >> elCnt;
+    assert(std::cin.good());
+
+    caches::cache_t<int> cache(size);
+
+    int insertable;
+    size_t hits = 0;
+    for (size_t i = 0; i != elCnt; ++i) {
+        std::cin >> insertable;
+        assert(std::cin.good());
+        hits += cache.lookup_update(insertable, ret);
+    }
+
+    std::cout << hits << '\n';
 }
